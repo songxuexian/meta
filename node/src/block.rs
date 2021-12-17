04faf63db41f5a4f53f4bb533f56cf6e08726418
songxuexian <s.xue.xian.c@gmail.com>
2021-12-17 09:59:46 +0800

    add trait but not finish

--- node/src/block.rs
+++ node/src/block.rs
@@ -4,10 +4,28 @@ use web3::{transports, Web3};
 use crate::connect;
 use crate::errors::NodeError;
 
+trait ChainNode {
+    type Output;
+    async fn get_block_by_hash(&self,hash:String) -> Result<Self::Output, NodeError>;
+}
+
 pub struct Node {
     _url: String,
     web3: Web3<transports::Http>,
 }
+
+impl ChainNode for Node{
+    type Output = Block<H256>;
+
+    async fn  get_block_by_hash(&self, hash:String) -> Result<Self::Output, NodeError> {
+        let hash = Hash::new(hash);
+        match self.web3.eth().block(BlockId::Hash(hash)).await.unwrap() {
+            Some(block) => return Ok(block),
+            None => return Err(NodeError::NotFoundHash(hash.to_string())),
+        }
+    }
+}
+
 impl Node {
     pub fn new(url: &str) -> Self {
         Node {
