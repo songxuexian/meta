ba2f42b0196fe604dcf547dc19701ff122a8a776
traitmeta <s.xue.xian.c@gmail.com>
2022-01-20 20:36:36 +0800

    change wshandler

--- wshandler/src/client.rs
+++ wshandler/src/client.rs
@@ -6,7 +6,6 @@ use crossbeam_channel::{bounded, tick, Receiver, Sender};
 use log::{debug, warn};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-use std::sync::Arc;
 use std::thread;
 use std::{fmt::Debug, time::Duration};
 use uuid::Uuid;
@@ -19,22 +18,23 @@ const MAX_SEND_CHAN: u64 = 1024;
 const MAX_SEND_CHAN_CAPACITY: u64 = MAX_SEND_CHAN + 128;
 
 static PING_FRAME: ClientCMD = ClientCMD {
-    action: String::from_utf8("ping"),
+    action: "ping".to_string(),
     args: Vec::new(),
-    client: None,
+    client_uuid: "".to_string(),
 };
 
-static PONG_FRAME: WSMessage = WSMessage::new("System".to_string(), 0, "pong".to_string());
+static PONG_FRAME: WSMessage = WSMessage::new("System", 0, "pong");
 
 #[derive(Deserialize, Serialize, Debug)]
-struct ClientCMD{
+struct ClientCMD {
     pub action: String,
     args: Vec<String>,
-    client: Arc<Client>,
+    client_uuid: String,
 }
 
 #[derive(Debug)]
 struct Client {
+    uuid: String,
     conn: WebSocket, //ws.Conn
     user: Option<User>,
     disconnected: bool,
@@ -45,6 +45,20 @@ struct Client {
 }
 
 impl Client {
+    fn new() -> Self {
+        let (s1, r1) = bounded(MAX_MESSAGE_SIZE as usize);
+        Client {
+            conn: todo!(),
+            user: None,
+            disconnected: false,
+            sender_channel: s1.clone(),
+            recv_channel: r1.clone(),
+            clientip: "".to_string(),
+            wall: ProtectiveWall::new(),
+            uuid: Uuid::new_v4().to_string(),
+        }
+    }
+
     fn set_channel(&self) {
         let (s1, r1) = bounded(MAX_MESSAGE_SIZE as usize);
         self.sender_channel = s1.clone();
@@ -92,7 +106,7 @@ impl Client {
         return self.clientip;
     }
 
-    async fn read_pump(&self) {
+    async fn read_pump(&mut self) {
         // todo need add auth ...
         loop {
             let cmd = self.do_socket_msg().await.unwrap();
@@ -101,12 +115,7 @@ impl Client {
         }
     }
 
-    async fn do_socket_msg(&self) -> Option<ClientCMD> {
-        let cmd = ClientCMD {
-            action: todo!(),
-            args: todo!(),
-            client: todo!(),
-        };
+    async fn do_socket_msg(&mut self) -> Option<ClientCMD> {
         if let Some(msg) = self.conn.recv().await {
             if let Ok(msg) = msg {
                 // todo parse msg to WSMessage, use json
@@ -118,8 +127,13 @@ impl Client {
         }
 
         let random_uuid = Uuid::new_v4();
-
-        debug!("accept message: {:?}, message id = {}", cmd, random_uuid);
+        let cmd = ClientCMD {
+            action: todo!(),
+            args: Vec::new(),
+            client_uuid: random_uuid.to_string(),
+        };
+        // todo! decode msg to WSMessage
+        debug!("accept message: {:?}, message id = {}", cmd, random_uuid.to_string());
         if cmd.action == PING_FRAME.action {
             debug!(
                 "respond to ping message={:?}, message id = %{}",
@@ -132,7 +146,7 @@ impl Client {
                 PONG_FRAME.data, random_uuid
             );
         }
-        cmd.client = *self;
+        cmd.client_uuid = self.uuid;
         Some(cmd)
     }
 
@@ -141,7 +155,7 @@ impl Client {
         // self.Close();
 
         loop {
-            thread::spawn(|| async {
+            let ticker_recv = thread::spawn(|| async {
                 ticker.recv().unwrap();
                 if self.disconnected {
                     debug!("send message message failed");
@@ -156,7 +170,7 @@ impl Client {
                     }
                 };
             });
-            thread::spawn(|| async {
+            let msg_recv = thread::spawn(|| async {
                 let msg = self.recv_channel.recv();
                 match msg {
                     Ok(msg) => {
@@ -167,7 +181,7 @@ impl Client {
                                 self.clientip, self.disconnected, msg
                             )
                         }
-                        match self.conn.send(Message::Text(json!(&msg))).await {
+                        match self.conn.send(Message::Text(json!(&msg).to_string())).await {
                             Ok(()) => (),
                             Err(err) => {
                                 warn!("Client {} WriteJSON error: {}", self.clientip, err);
@@ -182,6 +196,8 @@ impl Client {
                     }
                 }
             });
+            ticker_recv.join();
+            msg_recv.join();
         }
     }
 
@@ -189,7 +205,7 @@ impl Client {
         if self.disconnected {
             return false;
         }
-        thread::spawn(|| self.sender_channel.send(msg).unwrap());
+        // thread::spawn(move || self.sender_channel.send(msg).unwrap());
         true
         // select!{
         //  self.send <- msg:
--- wshandler/src/msg.rs
+++ wshandler/src/msg.rs
@@ -1,16 +1,15 @@
 use serde::{Deserialize, Serialize};
 
-
 #[derive(Serialize, Deserialize, Debug)]
 pub struct WSMessage {
     group: String,
     uid: i64,
-    data: String,
+    pub data: String,
 }
 
-impl WSMessage{
-    fn new(group: &str, uid: i64, data: &str)-> Self{
-        WSMessage{
+impl WSMessage {
+    pub fn new(group: &str, uid: i64, data: &str) -> Self {
+        WSMessage {
             group: group.to_string(),
             uid,
             data: data.to_string(),
