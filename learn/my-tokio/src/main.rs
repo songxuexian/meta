eb4a49c9faa1a6176e38a639f2037a1f12d4ca15
traitmeta <traitmeta@gmail.com>
2023-10-28 12:46:56 +0800

    use notify to impl delay

--- learn/my-tokio/src/main.rs
+++ learn/my-tokio/src/main.rs
@@ -1,6 +1,11 @@
-use std::time::{Duration, Instant};
+use std::{
+    sync::Arc,
+    thread,
+    time::{Duration, Instant},
+};
 
 use my_tokio::{future::Delay, tokio::MyTokio};
+use tokio::sync::Notify;
 
 #[tokio::main]
 async fn main() {
@@ -15,4 +20,22 @@ async fn main() {
     });
 
     my_tokio.run();
+}
+
+async fn delay(dur: Duration) {
+    let when = Instant::now() + dur;
+    let notify = Arc::new(Notify::new());
+    let notify2 = notify.clone();
+
+    thread::spawn(move || {
+        let now = Instant::now();
+
+        if now < when {
+            thread::sleep(when - now);
+        }
+
+        notify2.notify_one();
+    });
+
+    notify.notified().await;
 }
