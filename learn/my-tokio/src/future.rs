9c70fadeb063a912b831915bacfd6280889fa148
traitmeta <traitmeta@gmail.com>
2023-10-28 12:44:01 +0800

    add change waker if not exist

--- learn/my-tokio/src/future.rs
+++ learn/my-tokio/src/future.rs
@@ -1,28 +1,44 @@
 use std::pin::Pin;
-use std::task::{Context, Poll};
+use std::sync::Arc;
+use std::task::{Context, Poll, Waker};
 use std::thread;
 use std::{future::Future, time::Instant};
+
+use futures::lock::Mutex;
+
 pub struct Delay {
     pub when: Instant,
+    pub waker: Option<Arc<Mutex<Waker>>>,
 }
 
 impl Future for Delay {
     type Output = &'static str;
 
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        if Instant::now() >= self.when {
-            println!("Hello world!");
-            Poll::Ready("done")
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<&'static str> {
+        if let Some(waker) = &self.waker {
+            let mut waker = waker.try_lock().unwrap();
+            if !waker.will_wake(cx.waker()) {
+                *waker = cx.waker().clone();
+            }
         } else {
-            let waker = cx.waker().clone();
+            let waker = Arc::new(Mutex::new(cx.waker().clone()));
+            self.waker = Some(waker.clone());
             let when = self.when;
+
             thread::spawn(move || {
                 let now = Instant::now();
                 if now < when {
                     thread::sleep(when - now);
                 }
-                waker.wake();
+
+                let waker = waker.try_lock().unwrap();
+                waker.wake_by_ref();
             });
+        }
+
+        if Instant::now() >= self.when {
+            Poll::Ready("done")
+        } else {
             Poll::Pending
         }
     }
--- learn/my-tokio/src/main.rs
+++ learn/my-tokio/src/main.rs
@@ -4,13 +4,13 @@ use my_tokio::{future::Delay, tokio::MyTokio};
 
 #[tokio::main]
 async fn main() {
-    let mut my_tokio = MyTokio::new();
+    let my_tokio = MyTokio::new();
     my_tokio.swapn(async {
         let when = Instant::now() + Duration::from_secs(1);
-        let future = Delay { when };
+        let future = Delay { when, waker: None };
 
         let out = future.await;
-
+        println!("{}", out);
         assert!(out == "done")
     });
 
