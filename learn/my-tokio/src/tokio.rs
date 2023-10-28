749be64aa9f274bb8f19337ac07c6e9399d60424
traitmeta <traitmeta@gmail.com>
2023-10-28 12:00:25 +0800

    add channel in tokio for poll run

--- learn/my-tokio/src/tokio.rs
+++ learn/my-tokio/src/tokio.rs
@@ -2,38 +2,72 @@ use std::{
     collections::VecDeque,
     future::{self, Future},
     pin::Pin,
+    sync::Arc,
     task::Context,
 };
 
-use futures::task;
+use crossbeam::channel;
+use futures::{
+    lock::Mutex,
+    task::{self, ArcWake},
+};
 
 pub struct MyTokio {
-    tasks: VecDeque<Task>,
+    receiver: channel::Receiver<Arc<Task>>,
+    sender: channel::Sender<Arc<Task>>,
 }
 
-type Task = Pin<Box<dyn Future<Output = ()> + Send>>;
+struct Task {
+    future: Mutex<Pin<Box<dyn Future<Output = ()> + Send>>>,
+    executor: channel::Sender<Arc<Task>>,
+}
 
-impl MyTokio {
-    pub fn new() -> MyTokio {
-        MyTokio {
-            tasks: VecDeque::new(),
-        }
+impl Task {
+    fn schedule(self: &Arc<Self>) {
+        self.executor.send(self.clone());
     }
 
-    pub fn swapn<F>(&mut self, future: F)
+    fn poll(self: Arc<Self>) {
+        let waker = task::waker(self.clone());
+        let mut cx = Context::from_waker(&waker);
+        let mut future = self.future.try_lock().unwrap();
+        let _ = future.as_mut().poll(&mut cx);
+    }
+
+    fn spawn<F>(future: F, sender: &channel::Sender<Arc<Task>>)
     where
         F: Future<Output = ()> + Send + 'static,
     {
-        self.tasks.push_back(Box::pin(future));
+        let task = Arc::new(Task {
+            future: Mutex::new(Box::pin(future)),
+            executor: sender.clone(),
+        });
+
+        let _ = sender.send(task);
+    }
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        arc_self.schedule();
+    }
+}
+impl MyTokio {
+    pub fn new() -> MyTokio {
+        let (sender, scheduled) = channel::unbounded();
+        MyTokio { receiver: scheduled, sender }
     }
 
-    pub fn run(&mut self) {
-        let waker = task::noop_waker();
-        let mut cx = Context::from_waker(&waker);
-        while let Some(mut task) = self.tasks.pop_front() {
-            if task.as_mut().poll(&mut cx).is_pending() {
-                self.tasks.push_back(task);
-            }
+    pub fn swapn<F>(&self, future: F)
+    where
+        F: Future<Output = ()> + Send + 'static,
+    {
+        Task::spawn(future, &self.sender);
+    }
+
+    pub fn run(&self) {
+        while let Ok(task) = self.receiver.recv() {
+            task.poll();
         }
     }
 }
