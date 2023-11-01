5030632b5442ac9772d6ad5b098a4938c217f075
traitmeta <traitmeta@gmail.com>
2023-11-01 14:30:23 +0800

    feat: CMD => add publish

--- learn/my-redis/src/cmd/publish.rs
+++ learn/my-redis/src/cmd/publish.rs
@@ -1,15 +1,33 @@
+use bytes::Bytes;
+use tracing::debug;
+
 use crate::{
-    connection::{connect::Connection, error::ConnectionError},
-    storage::db::Db,
+    connection::{connect::Connection, error::ConnectionError, frame::Frame},
+    storage::{db::Db, kvstore::KvStore},
 };
 
 use super::CommandToFrame;
 
 #[derive(Debug)]
-pub struct Publish {}
+pub struct Publish {
+    channel: String,
+    message: Bytes,
+}
 
 impl Publish {
+    pub fn new(channel: impl ToString, message: Bytes) -> Publish {
+        Publish {
+            channel: channel.to_string(),
+            message,
+        }
+    }
     pub async fn apply(self, db: &Db, dst: &mut Connection) -> Result<(), ConnectionError> {
+        let num_subsribers = db.publish(&self.channel, self.message);
+
+        let response = Frame::Integer(num_subsribers as u64);
+        debug!("apply command applied response: {}", response);
+
+        dst.write_frame(&response).await?;
         Ok(())
     }
 }
@@ -20,12 +38,19 @@ impl CommandToFrame for Publish {
     fn parse_frames(
         parse: &mut crate::connection::parse::Parse,
     ) -> Result<Self::Output, crate::connection::error::ParseError> {
-        todo!()
+        let channel = parse.next_string()?;
+        let message = parse.next_bytes()?;
+        Ok(Publish { channel, message })
     }
 
     fn into_frame(
         self,
     ) -> Result<crate::connection::frame::Frame, crate::connection::error::ParseError> {
-        todo!()
+        let mut frame = Frame::array();
+        frame.push_bulk(Bytes::from("publish".as_bytes()))?;
+        frame.push_bulk(Bytes::from(self.channel.into_bytes()))?;
+        frame.push_bulk(Bytes::from(self.message))?;
+
+        Ok(frame)
     }
 }
